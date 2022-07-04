cfa6dd4105ac4ee7d2e4f3210d0e4130a598e524
João Victor Arouca <joao.silveira@estudante.ufla.br>
2022-07-04 17:56:51 -0300

    feat: fix script npm run dev

---
+++ Backend/src/Controller/TaskController.ts
@@ -0,0 +1,6 @@
+export default class TaskController {
+
+  public getAll = () => {
+    console.log('show');
+  }
+}
--- Backend/src/Routes/TaskRouter/index.ts
+++ Backend/src/Routes/TaskRouter/index.ts
@@ -1,5 +1,11 @@
 import { Router } from "express";
+/* import TaskController from "../../Controller/TaskController"; */
 
 const taskRouter = Router();
+
+/* const task = new TaskController(); */
+
+taskRouter.get('/', () => { console.log('cheguei');
+ })
 
 export default taskRouter;
